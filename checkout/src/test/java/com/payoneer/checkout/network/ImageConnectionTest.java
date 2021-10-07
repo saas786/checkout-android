d28200db3f22869182be875657599c128b982d9f
Fbada006 <ferdinandkamata@yahoo.com>
2021-10-07 16:55:00 +0300

    add tests for image connection class

---
+++ checkout/src/test/java/com/payoneer/checkout/network/ImageConnectionTest.java
@@ -0,0 +1,25 @@
+package com.payoneer.checkout.network;
+
+import android.graphics.Bitmap;
+import com.payoneer.checkout.core.PaymentException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+/**
+ * Containing ImageConnection tests
+ */
+@RunWith(RobolectricTestRunner.class)
+public class ImageConnectionTest {
+
+    /**
+     * Create ImageConnection and loadBitmap with invalid url
+     *
+     * @throws PaymentException the network exception
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void loadBitmap_invalidUrl_IllegalArgumentException() throws PaymentException {
+        ImageConnection conn = new ImageConnection();
+        Bitmap bitmap = conn.loadBitmap(null);
+    }
+}
