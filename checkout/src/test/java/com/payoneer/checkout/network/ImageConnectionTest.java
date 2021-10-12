ed1827d7e2cc6b1d2a90e7f34612043d9cbca994
Fbada006 <ferdinandkamata@yahoo.com>
2021-10-12 09:31:40 +0300

    update tests to use jupiter instead of roboelectric

--- checkout/src/test/java/com/payoneer/checkout/network/ImageConnectionTest.java
+++ checkout/src/test/java/com/payoneer/checkout/network/ImageConnectionTest.java
@@ -11,25 +11,18 @@
 package com.payoneer.checkout.network;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
-
-import com.payoneer.checkout.core.PaymentException;
+import org.junit.jupiter.api.Assertions;
 
 /**
  * Containing ImageConnection tests
  */
-@RunWith(RobolectricTestRunner.class)
 public class ImageConnectionTest {
 
-    /**
-     * Create ImageConnection and loadBitmap with invalid url
-     *
-     * @throws PaymentException the network exception
-     */
-    @Test(expected = IllegalArgumentException.class)
-    public void loadBitmap_invalidUrl_IllegalArgumentException() throws PaymentException {
-        ImageConnection conn = new ImageConnection();
-        conn.loadBitmap(null);
+    @Test()
+    public void loadBitmap_invalidUrl_IllegalArgumentException() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            ImageConnection conn = new ImageConnection();
+            conn.loadBitmap(null);
+        });
     }
 }
