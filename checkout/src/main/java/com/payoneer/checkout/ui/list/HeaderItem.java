30ba449bdb9c3eb9dfe8d73369215901989be07d
Ferdinand Bada <33724380+Fbada006@users.noreply.github.com>
2021-10-23 10:59:55 +0300

    PCX-2228: add preset warning to the header item and list

--- checkout/src/main/java/com/payoneer/checkout/ui/list/HeaderItem.java
+++ checkout/src/main/java/com/payoneer/checkout/ui/list/HeaderItem.java
@@ -13,10 +13,20 @@ package com.payoneer.checkout.ui.list;
  */
 final class HeaderItem extends ListItem {
 
-    final String title;
+    private final String title;
+    private final String presetWarning;
 
-    HeaderItem(int viewType, String title) {
+    HeaderItem(int viewType, String title, String presetWarning) {
         super(viewType);
         this.title = title;
+        this.presetWarning = presetWarning;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPresetWarning() {
+        return presetWarning;
     }
 }
